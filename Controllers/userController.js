4b82b5c89afb676e90eb5265ce2eeeb1c8606f23
subzero1221 <bkelekhsaevi@gmail.com>
2024-08-07 00:20:23 +0400

    update Drive

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -24,11 +24,23 @@ exports.getUsers = catchAsync(async (req, res, next) => {
   });
 });
 
-const KEYFILEPATH = path.resolve(__dirname, process.env.KEYFILEPATH);
+const apiKey = {
+  type: "service_account",
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+};
 
 async function getAuthClient() {
   const auth = new google.auth.GoogleAuth({
-    keyFile: KEYFILEPATH,
+    credentials: apiKey,
     scopes: process.env.SCOPE,
   });
   return await auth.getClient(); // Return the authorized client
